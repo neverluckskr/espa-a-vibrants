be8929fab7fb87f2cfbcedb81cc5b79c60feff96
pain in my heart <vladvovk2010@gmail.com>
2025-11-30 15:55:38 +0300

    fix bugs for mobile devices

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -79,6 +79,7 @@ const Timeline = () => {
             </span>
             <span>Анна · Хронологія</span>
           </div>
+        </div>
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
