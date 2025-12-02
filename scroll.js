42ec6b80cc35b2b02680cf6a4b6729e6113d324e
emocionale deprimes <vladvovk2010@gmail.com>
2025-12-02 09:52:35 +0200

    Updated scroll.js with new scroll functionality

--- scroll.js
+++ scroll.js
@@ -8,3 +8,23 @@ document.addEventListener("keydown", function(event) {
         window.scrollBy(0, -100);
     }
 });
+
+document.addEventListener("touchstart", function(event) {
+    // Определяем область касания для мобильных устройств
+    let startY = 0;
+    event.preventDefault(); // Отключает прокрутку браузером
+    if (event.changedTouches && event.changedTouches[0]) {
+        startY = event.changedTouches[0].clientY;
+    }
+
+    document.addEventListener("touchend", function(e) {
+        if (e.changedTouches && e.changedTouches[0]) {
+            let diffY = e.changedTouches[0].clientY - startY;
+            if (diffY < -30) {
+                window.scrollBy(0, 100); // Вниз
+            } else if (diffY > 30) {
+                window.scrollBy(0, -100); // Вверх
+            }
+        }
+    }, {once: true});
+}, {passive: false});
